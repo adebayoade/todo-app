5377758683fae4f285c4a2ab335bcb6e3ca961b2
Adebayo Ade <vanbayor@gmail.com>
2024-10-16 07:59:13 +0100

    removed unused packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routes';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from './providers/theme-provider';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
 const router = createBrowserRouter(routes);
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
     <Provider store={store}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
   );
